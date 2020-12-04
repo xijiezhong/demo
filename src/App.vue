ec2a79a5910585707c714df81c2fdc3b4a3c98b5
xijie <xijie205@163.com>
2020-12-04 14:17:05 +0800

    加入横屏显示

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,39 @@
   </div>
 </template>
 
+<script>
+export default {
+  created () {
+    window.onresize = this.detectOrient
+    this.detectOrient()
+  },
+  methods: {
+    detectOrient () {
+      const width = document.documentElement.clientWidth
+      const height = document.documentElement.clientHeight
+      const $wrapper = document.getElementById('app')
+      let style = ''
+
+      if (width >= height) { // 横屏
+        style += 'width:' + width + 'px;' // 注意旋转后的宽高切换
+        style += 'height:' + height + 'px;'
+        style += '-webkit-transform: rotate(0); transform: rotate(0);'
+        style += '-webkit-transform-origin: 0 0;'
+        style += 'transform-origin: 0 0;'
+      } else { // 竖屏
+        style += 'width:' + height + 'px;'
+        style += 'height:' + width + 'px;'
+        style += '-webkit-transform: rotate(90deg); transform: rotate(90deg);'
+        // 注意旋转中点的处理
+        style += '-webkit-transform-origin: ' + width / 2 + 'px ' + width / 2 + 'px;'
+        style += 'transform-origin: ' + width / 2 + 'px ' + width / 2 + 'px;'
+      }
+      $wrapper.style.cssText = style
+    }
+  }
+}
+</script>
+
 <style>
 *{
   margin: 0;
